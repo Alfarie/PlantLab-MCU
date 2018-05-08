0084ed3a2eaa13f9b00a8c9e51577e90bf0e8a0c
worawish <worawish.k@gmail.com>
2018-05-09 03:17:18 +0700

    define output pins

--- PlantLab-MCU.ino
+++ PlantLab-MCU.ino
@@ -8,6 +8,12 @@ TaskManager taskManager;
 #define VERSION 2.0
 #define PROJECT "PLANTLAB-GROBOT"
 #define UPLOADDATE String(__DATE__) + " " +  String(__TIME__)
+#define ledPin 2
+#define co2Pin 4
+#define ecPin 6
+#define phPin 7
+#define pumpPin 3
+#define valvePin 5
 
 String ShowBoardInfo(){
     String str = "INFOBOARD-VERSION" + String(VERSION) + "\r\n";
@@ -51,7 +57,7 @@ struct sensor_s
 Control *channel[CHANNEL_NUMBER];
 
 //
-int ChannelGpio[CHANNEL_NUMBER] = {2,4,6,7,3,5};//LED,CO2,EC,PH,PUMP,VALVE
+int ChannelGpio[CHANNEL_NUMBER] = {ledPin,co2Pin,ecPin,phPin,pumpPin,valvePin};
 int ChannelStatus[CHANNEL_NUMBER] = {LOW,LOW,LOW,LOW,LOW,LOW};
 
 void DigitalWrite(int ch, int status){
