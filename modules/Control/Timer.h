f8f3de64165fdaf4287ec4b007ec541bcf7f1f22
Farhan Abuhazan <farhan.abuhazan@gmail.com>
2018-05-13 20:13:32 +0700

    New timer

--- modules/Control/Timer.h
+++ modules/Control/Timer.h
@@ -3,78 +3,119 @@
 extern TaskManager taskManager;
 class Timer : public Control
 {
-  public:
-    Timer(int ch) : Control(1000,ch){
-      UpdateFromRom();
-    };
+public:
+  Timer(int ch) : Control(1000, ch)
+  {
+    UpdateFromRom();
+  };
 
-    void SaveValue(timer_s *t, int s,int m )
+  void SaveValue(timer_s *t, int s, int m)
+  {
+    ArrayTimerCopy(timer_list, t, s);
+    size = s;
+    mode = m;
+    SaveToRom();
+  }
+
+private:
+  timer_s timer_list[8];
+  byte size;
+  byte mode;
+  byte state = 0;
+  float crt = 0, working = 10, crtMin = 0;
+  virtual bool OnStart()
+  {
+    DigitalWrite(channel - 1, CH_OFF);
+    return true;
+  }
+  // datetime,22,2,2018,14,21}
+  virtual void OnUpdate(uint32_t delta_time)
+  {
+    int currentMin = RTC::instance()->GetCurrentMin();
+    // debugCom.print("Timer    is running " + String(channel) + " " + String(mode) + " " + String(size)) + " " + String(currentMin);
+    // ;
+    // debugCom.print(" [");
+    // for (int i = 0; i < size; i++)
+    // {
+    //   debugCom.print(String(timer_list[i].st) + "-" + String(timer_list[i].en) + ",");
+    // }
+    // debugCom.println("] ");
+
+    if (mode == 0)
     {
-      ArrayTimerCopy(timer_list, t,s);
-      size = s;
-      mode = m;
-      SaveToRom();
-    }
-  private:
-    timer_s timer_list[8];
-    byte size;
-    byte mode;
-    virtual bool OnStart()
-    {
-      DigitalWrite(channel - 1, CH_OFF);
-      return true;
-    }
-    // datetime,22,2,2018,14,21}
-    virtual void OnUpdate(uint32_t delta_time)
-    {
-      int currentMin = RTC::instance()->GetCurrentMin();
-       debugCom.print("Timer    is running " + String(channel) + " " + String(mode) + " " + String(size)) + " " + String(currentMin);;
-       debugCom.print(" [");
-       for(int i = 0 ; i < size; i++)
-       {
-         debugCom.print( String(timer_list[i].st) + "-" + String(timer_list[i].en) + ",");
-       }
-       //debugCom.println("] ");
-     
       bool flag = false;
-      for(int i = 0 ; i < size; i++){
-        if(currentMin >= timer_list[i].st && currentMin <= timer_list[i].en){
+      for (int i = 0; i < size; i++)
+      {
+        if (currentMin >= timer_list[i].st && currentMin <= timer_list[i].en)
+        {
           flag = true;
           break;
         }
       }
-      if(flag){
-        // ChannelHanler::SetChannelStatus( channel - 1, HIGH);
+      if (flag)
+      {
         DigitalWrite(channel - 1, CH_ON);
-        // //debugCom.println(" CH_ON ");
       }
-      else {
-        // ChannelHanler::SetChannelStatus( channel - 1, LOW);
+      else
+      {
         DigitalWrite(channel - 1, CH_OFF);
-        // //debugCom.println(" CH_OFF ");
       }
     }
 
-    void ArrayTimerCopy(timer_s *from, timer_s *to, int size)
+    else if (mode == 1)
     {
-      int i = 0 ;
-      for( i = 0 ; i < size ; i++){
-        to[i] = from[i];
+      bool flag = false;
+      //not yet
+      if (state == 0)
+      {
+        for (int i = 0; i < size; i++)
+        {
+          if (currentMin == timer_list[i].st)
+          {
+            flag = true;
+            state = 1; // doing
+            working = (float)timer_list[i].en;
+            crtMin = (float)timer_list[i].st;
+            DigitalWrite(channel - 1, CH_ON);
+            break;
+          }
+        }
+      }
+      else if(state == 1){
+        crt += (delta_time/1000.0);
+        if(crt >= working){
+          DigitalWrite(channel - 1, CH_OFF);
+          state = 3;
+          crt = 0;
+        }
+      }
+      else if(state == 3){
+        if(currentMin > crtMin){
+          state = 0;
+        }
       }
     }
-
-    void UpdateFromRom()
+  }
+  void ArrayTimerCopy(timer_s *from, timer_s *to, int size)
+  {
+    int i = 0;
+    for (i = 0; i < size; i++)
     {
-      ArrayTimerCopy(rom_channel[channel-1].timer.timer_list, timer_list, rom_channel[channel-1].timer.size);
-      mode = rom_channel[channel-1].timer.mode;
-      size = rom_channel[channel-1].timer.size;
+      to[i] = from[i];
     }
+  }
 
-    void SaveToRom()
-    {
-      ArrayTimerCopy(timer_list, rom_channel[channel-1].timer.timer_list, rom_channel[channel-1].timer.size);
-      rom_channel[channel-1].timer.mode = mode;
-      rom_channel[channel-1].timer.size = size;
-    }
+  void UpdateFromRom()
+  {
+    ArrayTimerCopy(rom_channel[channel - 1].timer.timer_list, timer_list, rom_channel[channel - 1].timer.size);
+    mode = rom_channel[channel - 1].timer.mode;
+    size = rom_channel[channel - 1].timer.size;
+  }
 
+  void SaveToRom()
+  {
+    ArrayTimerCopy(timer_list, rom_channel[channel - 1].timer.timer_list, rom_channel[channel - 1].timer.size);
+    rom_channel[channel - 1].timer.mode = mode;
+    rom_channel[channel - 1].timer.size = size;
+  }
 };
